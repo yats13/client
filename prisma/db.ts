1e72693720f36408b2ce7845f9f6437f6636fc89
Andrii Yatskovskyi <andrii@itsharkz.com>
2025-01-31 19:20:57 +0100

    Update psychologist

--- prisma/db.ts
+++ prisma/db.ts
@@ -4,6 +4,4 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
 const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
 export default prisma;
